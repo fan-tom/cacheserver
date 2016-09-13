4202bb2a70757974dc91837784f81df1e6d5f9c3
Alexey Gerasimov <lokomot476@mail.ru>
2016-09-13 11:27:22 +0500

    Fix RPS benchmark sockets exhaustion

--- benchmark/benchmark.go
+++ benchmark/benchmark.go
@@ -40,26 +40,33 @@ func main() {
 	for {
 		switch rand.Int() % 4{
 		case GET:
-			client.Get(URL+"/"+ids.Front().Value.(string))
+			resp,err=client.Get(URL+"/"+ids.Front().Value.(string))
 			log.Println("GET")
+			if err==nil {
+				resp.Body.Close()
+			}
 		case SET:
-			resp,err:=client.Post(URL,"text/json",strings.NewReader(value))
+			resp,err=client.Post(URL,"text/json",strings.NewReader(value))
 			if err==nil {
 				ids.PushFront(readBody(resp))
 			}
 			log.Println("SET")
 		case UPDATE:
 			req,_:=http.NewRequest(http.MethodPut,URL+"/"+ids.Front().Value.(string),strings.NewReader(value))
-			client.Do(req)
-			resp.Body.Close()
+			resp,err=client.Do(req)
+			if err==nil {
+				resp.Body.Close()
+			}
 			log.Println("UPDATE")
 		case DELETE:
 			req,_:=http.NewRequest(http.MethodDelete,URL+"/"+ids.Front().Value.(string),nil)
-			client.Do(req)
-			resp.Body.Close()
+			resp,err=client.Do(req)
+			if err==nil {
+				resp.Body.Close()
+			}
 			log.Println("DELETE")
 		}
-		resp,err:=client.Get("http://localhost:8080/api/metrics/rps")
+		resp,err=client.Get("http://localhost:8080/api/metrics/rps")
 		if err==nil {
 			log.Println(readBody(resp))
 		} else {
